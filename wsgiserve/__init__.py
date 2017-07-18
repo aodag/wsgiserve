7735955a76187ef826e6479ae24b4e49cf911f72
Atsushi Odagiri <aodagx@gmail.com>
2017-07-19 03:40:18 +0900

    add doc strings

--- wsgiserve/__init__.py
+++ wsgiserve/__init__.py
@@ -7,35 +7,43 @@ logger = logging.getLogger(__name__)
 
 
 class Command(object):
+    """command of wsgiserve"""
+
     def __init__(self, config, reload):
         self.config = config
         self.reload = reload
 
     def setup_logging(self):
+        """call setup_logging for self's config"""
         plaster.setup_logging(self.config)
 
     def setup_reloader(self):
+        """setup reloader if reload option specified"""
         if self.reload:
             reloader = hupper.start_reloader("wsgiserve.main")
             reloader.watch_files([self.config])
 
     def get_wsgi_loader(self):
+        """get wsgi loader from self's config"""
         loader = plaster.get_loader(self.config, protocols=["wsgi"])
         return loader
 
     def run(self):
+        """run command"""
         self.setup_logging()
         self.setup_reloader()
         loader = self.get_wsgi_loader()
         self.run_wsgi(loader)
 
     def run_wsgi(self, loader):
+        """serve wsgi app with loader"""
         app = loader.get_wsgi_app()
         server = loader.get_wsgi_server()
         server(app)
 
 
 def setup_arguments(parser):
+    """setup arguments for parser"""
     parser.add_argument("config",
                         help="A path to the configuration file")
     parser.add_argument("--reload", action="store_true",
@@ -43,6 +51,7 @@ def setup_arguments(parser):
 
 
 def main():
+    """main procedure"""
     parser = argparse.ArgumentParser()
     setup_arguments(parser)
     args = parser.parse_args()
